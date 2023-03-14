a8d436082ed010473a1c8bd0d4e0fbdee6fbb036
Pablo Martinez <pablomartinez555@hotmail.com>
2023-03-14 16:43:55 -0300

    Add KeyDown Functionality

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -4,7 +4,17 @@ interface FormInputProps {
   onSearchClickHandle: () => void;
 }
 
-  const FormInput = ({value, onInputChange, onSearchClickHandle }: FormInputProps) => {
+const FormInput = ({ value, onInputChange, onSearchClickHandle }: FormInputProps) => {
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      onSearchClickHandle();
+    }
+  }
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onInputChange(event.target.value)
+  }  
 
   return (
     <div className="w-full h-9 my-5 mx-0 flex">
@@ -15,7 +25,8 @@ interface FormInputProps {
         required
         placeholder="Artist Name"
         value={value}
-        onChange={e => onInputChange(e.target.value)}
+        onChange={handleOnChange}
+        onKeyDown={handleKeyDown}
       />
       <button
         className="flex-1 outline-none my-0 bg-gray-500 text-white tracking-widest cursor-pointer rounded-r-md disabled:cursor-not-allowed hover:bg-gray-600 disabled:hover:bg-gray-500"
